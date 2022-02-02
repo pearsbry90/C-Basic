f47740af817ae20354551e8eb0ea4c061f7ce05b
Bryon <pearson.bryon90@gmail.com>
2022-02-02 11:11:52 -0800

    adding 02_Types to Fundamental Solution/Project

--- 02_Types/Program.cs
+++ 02_Types/Program.cs
@@ -6,7 +6,28 @@ namespace BasicTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+/* bool is a true false value, no quotes around the word */ 
+// this is b/c true and false are keywords, not strings
+bool isLoggedIn = true; 
+Console.WriteLine(isLoggedIn);
+
+// integers, or int, are whole numbers only
+int age = 42;
+Console.WriteLine(age);
+
+double balanceDue = 800.99d;
+Console.WriteLine(balanceDue);
+
+// decimal has a lot of possible decimal points
+// it also needs the m suffix to know it's a decimal type
+decimal accountBalance = 9000.01m;
+Console.WriteLine(accountBalance);
+
+// another useage of decimal; memory usages; giving a range of a number
+float averageHeight = 5.7f;
+Console.WriteLine(averageHeight);
+
+// Notice that when the accountBalance gets written to the console there is no m suffix in the output
+        } 
     }
 }
