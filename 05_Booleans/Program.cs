218087330a1b3a227b247df07ca072ad160ca056
Bryon <pearson.bryon90@gmail.com>
2022-02-04 10:12:14 -0800

    adding 05_Booleans

---
+++ 05_Booleans/Program.cs
@@ -0,0 +1,25 @@
+﻿// Boolean Logical Operators
+bool isCool = false;
+isCool = true; 
+
+// Backwords saying
+bool isTrue = !false;
+bool isFalse = !true;
+
+// AND Operator
+// A part of the operator has to be false as well
+bool trueANDExample = true && true;
+bool falseANDExample = true && false;
+
+// OR Operator
+bool trueOrExample = true || false;
+bool falseOrExample = false || false;
+
+// XOR Operator
+bool trueExclusiveOr = true ^ false;
+bool falseExclusiveOr = true ^ true;
+
+Console.WriteLine($"NOT False = {isTrue}");
+Console.WriteLine($"False AND True = {falseANDExample}");
+Console.WriteLine($"True OR True = {trueOrExample}");
+Console.WriteLine($"True XOR True = {falseExclusiveOr}"); 
