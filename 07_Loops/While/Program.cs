0fc19effa99a66216947bbe087d4008724e5a9a5
Bryon <pearson.bryon90@gmail.com>
2022-02-07 09:58:05 -0800

    deleted files on cssBasics

---
+++ 07_Loops/While/Program.cs
@@ -0,0 +1,15 @@
+﻿// WhileWhile loops execute a statement or a block of statements while the specified boolean expression evaluates to TRUE
+// Meaning, giving the while loop a boolean to evaluate: TRUE loops; FALSE does not
+// Ex. If you're hungry, you should eat. If you're not hungry, you should stop eating.
+
+// Delcare a variable
+int number = 0;
+
+// Keyword Condition
+while (number <= 100) 
+{
+    // Loop
+    // Print the number and add 5
+    Console.WriteLine(number);
+    number = number + 5;
+}
